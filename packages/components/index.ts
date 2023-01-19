0c17aa362c72c848e29a22c4603af5e5284f5ee0
zhangshuai <1016868503@qq.com>
2023-01-19 22:23:54 +0800

    测试4

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -2,7 +2,6 @@ import components from './src/index';
 export * from './src/index';
 import type { App } from 'vue';
 import { version } from './package.json'; 
-
 const install = function (app: App): void {
   Object.entries(components).forEach(([key, value]) => {
     app.component(key, value);
